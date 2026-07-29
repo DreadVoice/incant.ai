83962962786aee4a06de271bb5f29a4a5c4a79e1
122AgA5! <akashchoudhary2005@gmail.com>
2026-07-29 23:02:06 +0530

    chore: Initialise Spring Boot 3 project with gradle

---
+++ src/main/java/io/github/dreadvoice/incant/IncantApplication.java
@@ -0,0 +1,13 @@
+package io.github.dreadvoice.incant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/dreadvoice/incant/IncantApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dreadvoice.incant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
